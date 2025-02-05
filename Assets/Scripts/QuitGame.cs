c400d4be0a40af3406198c6e5208f21d135efa04
osamahussein2 <osamahusseinn2@gmail.com>
2025-02-05 13:40:01 -0500

    Quit button works

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    public void PressQuitButton()
+    {
+        // Quit the game after pressing the quit button
+        Application.Quit();
+
+        // Quit the game inside the editor too
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
