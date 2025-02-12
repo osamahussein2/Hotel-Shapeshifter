be7714625e9ce1e326bf7fbd481602611d079601
osamahussein2 <osamahusseinn2@gmail.com>
2025-02-12 12:55:26 -0500

    Determines if the player can finish side quest

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,4 +4,6 @@ public class GameState : MonoBehaviour
 {
     public int dialogueProgress = 0; // Tracks progress of game
     public int time = 0;
+
+    public static bool didPlayerFinishSideQuest = false; // Make it so that the player can finish the side quest
 }
