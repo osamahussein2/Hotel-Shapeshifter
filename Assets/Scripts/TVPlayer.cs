55831706e2cb24e0d37d8b4084cdbc53de07411f
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2025-03-27 22:19:23 -0400

    Re-Fixed Journal. Made TV

---
+++ Assets/Scripts/TVPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVPlayer : MonoBehaviour
+{
+    public GameState State;
+    public GameObject TV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(State.currentDay == 2)
+        {
+            NewsBroadcast();
+        }
+    }
+
+    public void NewsBroadcast()
+    {
+        TV.SetActive(true);
+    }
+
+
+
+}
