5cf5d2dcc76ed6d7ea2b6191673a6b6362b36658
ironpenguin222 <148375713+ironpenguin222@users.noreply.github.com>
2025-03-29 14:24:16 -0400

    More dialogue, bug fixes, and new room

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using static DialogueManager;
+using UnityEngine.EventSystems;
 
 public class DialogueTrigger : MonoBehaviour
 {
@@ -21,28 +22,32 @@ public class DialogueTrigger : MonoBehaviour
     private void OnMouseDown()
     {
         // If dialogue isn't already active, start it
-        if (!dialogueManager.dialogueActive)
-        {
-            // Check if the current quest progress is within the bounds of the list
-            if (gameState.dialogueProgress >= 0 && gameState.dialogueProgress < dialogueSections.Count)
-            {
-                // Load the dialogue nodes for the current section
-                dialogueManager.SetDialogueNodes(dialogueSections[gameState.dialogueProgress].dialogueNodes);
-                var data = new CharacterInteractData()
-                {
-                    playerPos = transform.position,
-                    character = character
-                };
-                TelemetryLogger.Log(this, "Dialogue Started", data);
-            }
-            else
-            {
-                Debug.Log("No dialogue section found for  progress: " + gameState.dialogueProgress);
-                return;
-            }
 
-            dialogueManager.SetCurrentCharacter(character); // Set the character
-            dialogueManager.StartDialogue(); // Start the dialogue
+        if (!EventSystem.current.IsPointerOverGameObject()) // Ensures UI blocks game interaction
+        {
+            if (!dialogueManager.dialogueActive)
+            {
+                // Check if the current quest progress is within the bounds of the list
+                if (gameState.dialogueProgress >= 0 && gameState.dialogueProgress < dialogueSections.Count)
+                {
+                    // Load the dialogue nodes for the current section
+                    dialogueManager.SetDialogueNodes(dialogueSections[gameState.dialogueProgress].dialogueNodes);
+                    var data = new CharacterInteractData()
+                    {
+                        playerPos = transform.position,
+                        character = character
+                    };
+                    TelemetryLogger.Log(this, "Dialogue Started", data);
+                }
+                else
+                {
+                    Debug.Log("No dialogue section found for  progress: " + gameState.dialogueProgress);
+                    return;
+                }
+
+                dialogueManager.SetCurrentCharacter(character); // Set the character
+                dialogueManager.StartDialogue(); // Start the dialogue
+            }
         }
     }
 }
