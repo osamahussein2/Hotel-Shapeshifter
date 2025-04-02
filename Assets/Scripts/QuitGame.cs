c022b624a7f65cc3a699364570bdfaddb0854a01
osamahussein2 <osamahusseinn2@gmail.com>
2025-04-02 11:29:54 -0400

    Added a quit confirmation UI after pressing the quit button

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -5,12 +5,36 @@ using UnityEngine.UI;
 
 public class QuitGame : MonoBehaviour
 {
+    public Canvas inGame;
+    public Canvas quitConfirmation;
+
+    public static bool paused;
+
+    private void Start()
+    {
+        inGame.gameObject.SetActive(true);
+        quitConfirmation.gameObject.SetActive(false);
+    }
+
     public void PressQuitButton()
+    {
+        inGame.gameObject.SetActive(false);
+        quitConfirmation.gameObject.SetActive(true);
+
+        paused = true;
+    }
+
+    public void PressYesButton()
     {
         // Quit the game after pressing the quit button
         Application.Quit();
+    }
 
-        // Quit the game inside the editor too
-        //UnityEditor.EditorApplication.isPlaying = false;
+    public void PressNoButton()
+    {
+        inGame.gameObject.SetActive(true);
+        quitConfirmation.gameObject.SetActive(false);
+
+        paused = false;
     }
 }
